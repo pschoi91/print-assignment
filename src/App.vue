df970eafdc865d7a3da107b60715bc4656facc39
Sanne Choi <sanne.choi@incentro.com>
2024-08-07 17:47:41 +0200

    feat(ProductListing): create product tabs

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import ProductListingView from '@/views/ProductListingView.vue'
+
+defineOptions({
+  name: 'App',
+  components: { ProductListingView }
+})
 </script>
 
 <template>
- <div></div>
+ <v-app>
+  <v-container>
+    <v-app-bar title="Catalogue" />
+    <v-main app>
+      <product-listing-view />
+    </v-main>
+  </v-container>
+ </v-app>
 </template>
 
 <style scoped>
---
+++ src/views/ProductListingView.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { useCatalogueStore } from '@/stores/catalogue';
+import { storeToRefs } from 'pinia';
+
+const { products } = storeToRefs(useCatalogueStore())
+
+defineOptions({
+    name: "ProductListingView",
+    data: () => {
+        return {
+            productTab: null
+        }
+    }
+})
+
+</script>
+
+<template>
+    <v-container>
+        <v-tabs 
+            v-model="productTab"
+            align-tabs="center"
+            bg-color="deep-purple-accent-4"
+            stacked
+        >
+            <v-tab :value="products[0]?.sku" :text="products[0]?.title">
+                
+            </v-tab>
+            <v-tab :value="products[1]?.sku" :text="products[1]?.title">
+                
+            </v-tab>
+            <v-tab :value="products[2]?.sku" :text="products[2]?.title">
+                
+            </v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="productTab">
+            <v-tabs-window-item :key="products[0]?.sku" :value="products[0]?.sku">
+                <v-card>
+                    <v-card-text>{{ products[0]?.properties }}</v-card-text>
+                </v-card>
+            </v-tabs-window-item>
+            <v-tabs-window-item :key="products[1]?.sku" :value="products[1]?.sku">
+                <v-card>
+                    <v-card-text>{{  products[1]?.properties}}</v-card-text>
+                </v-card>
+            </v-tabs-window-item>
+            <v-tabs-window-item :key="products[2]?.sku" :value="products[2]?.sku">
+                <v-card>
+                    <v-card-text>{{ products[2]?.properties }}</v-card-text>
+                </v-card>
+            </v-tabs-window-item>
+        </v-tabs-window>
+    </v-container>
+</template>
+
+<style scoped>
+
+</style>
